1c151c893057eef029c800d27f5c7f4ef608040f
Lukasz Sreniawski <sreniok@gmail.com>
2020-01-13 19:27:50 +0000

    setup port

--- env.py
+++ env.py
@@ -1,2 +1,5 @@
 import os 
 os.environ["SECRET_KEY"] = 'mongodb+srv://Luca:sreniawski85@myfirstcluster0-3nxsy.mongodb.net/food_recipe?retryWrites=true&w=majority'
+
+SECRET_KEY = os.environ.get('SECRET_KEY') 
+app.config['MONGO_URI'] =  SECRET_KEY
